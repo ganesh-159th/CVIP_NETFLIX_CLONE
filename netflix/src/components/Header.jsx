0d8c4e4496a9f4f9d66aa9259e699a71df4864c4
ganesh <ganeshkumar159th@gmail.com>
2023-11-30 19:57:01 +0530

    commit number1

---
+++ netflix/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import styled from 'styled-components';
+import logo from '../assest/logo.png';
+import {  useNavigate } from 'react-router-dom';
+
+
+
+export default function Header(props) {
+    const navigate = useNavigate();
+  return (
+    <StyledHeader className='flex a-center j-between '>
+        <div className="logo">
+        <img src={logo} alt="logo" />
+        </div>
+        <button onClick={() => navigate(props.login ? "/login" : "/Signup")}>
+            {props.login ? "Log In " :" Sign In"}
+       </button>
+   
+    </StyledHeader>
+  );
+}
+const StyledHeader = styled.header`
+padding: 0 4rem;
+.logo {
+  img {
+    height: 3rem;
+  }
+}
+button {
+  padding: 0.5rem 1rem;
+  background-color: #e50914;
+  border: none;
+  cursor: pointer;
+  color: white;
+  border-radius: 0.2rem;
+  font-weight: bolder;
+  font-size: 1.05rem;
+}
+`;
